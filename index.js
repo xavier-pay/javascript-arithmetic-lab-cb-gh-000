185214da2bf4d3d2da0b2f7ac559cc766243a2bb
Aurelien Xavier Payong D <xavier.payong@gmail.com>
2018-01-12 16:58:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+function add(a,b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide(a, b){
+  return a / b
+}
+
+function inc(n){
+  n += 1
+  return n
+}
+
+function dec(n){
+  n -= 1
+  return n
+}
+
+function makeInt(n){
+  if(typeof(n) != "integer"){
+    return parseInt(n, 10)
+  }else{
+    return NaN
+  }
+}
+
+function preserveDecimal(n){
+  if(typeof(n) != "float"){
+    return parseFloat(n)
+  }else{
+    return NaN
+  }
+}
